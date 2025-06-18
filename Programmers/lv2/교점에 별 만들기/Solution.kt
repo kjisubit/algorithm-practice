d0d0186e7141b565fd84c64cb6daf0591833a445
Jisu Kim <kjisubit@gmail.com>
2025-06-18 22:27:40 +0900

    chore: 문제 풀이 개선

--- Programmers/lv2/교점에 별 만들기/Solution.kt
+++ Programmers/lv2/교점에 별 만들기/Solution.kt
@@ -1,23 +1,23 @@
-// 클래스로 좌표 표현
-// 교차점 리턴하는 함수 만들기 (정수로만)
-// line 의 모든 원소에 대하여 교차점 구하여 어레이리스트에 저장
-// (구한 교차점을 최소한의 사각형으로 표현해야 함)
-// x 최대 길이, y 최대 길이로 구성된 정사각형 만들기
-// 사각형에 교차점 표시
-// 사각형의 각 행을 문자열로 만들어 어레이에 저장
+// 좌표는 클래스로 표현
+// 메서드 1 -> 교차점 리턴
+// 메서드 2 -> 최소 좌표 리턴
+// 메서드 3 -> 최대 좌표 리턴
+// 마지막에 사각형 리턴
 
 class Solution {
     private class Point(val x: Long, val y: Long)
 
+    // 교차점 리턴
     private fun intersection(a: Long, b: Long, e: Long, c: Long, d: Long, f: Long): Point? {
         val x = (b * f - e * d).toDouble() / (a * d - b * c)
         val y = (e * c - a * f).toDouble() / (a * d - b * c)
 
-        if (x % 1 != 0.0 || y % 1 != 0.0) return null
-        return Point(x.toLong(), y.toLong())
+        if (x % 1 == 0.0 && y % 1 == 0.0) return Point(x.toLong(), y.toLong())
+        return null
     }
 
-    private fun getMinimumPoint(points: List<Point>): Point {
+    // 최소 좌표 리턴
+    private fun getMin(points: List<Point>): Point {
         var x = Long.MAX_VALUE
         var y = Long.MAX_VALUE
 
@@ -25,10 +25,12 @@ class Solution {
             if (p.x < x) x = p.x
             if (p.y < y) y = p.y
         }
+
         return Point(x, y)
     }
 
-    private fun getMaximumPoint(points: List<Point>): Point {
+    // 최대 좌표 리턴
+    private fun getMax(points: List<Point>): Point {
         var x = Long.MIN_VALUE
         var y = Long.MIN_VALUE
 
@@ -36,15 +38,16 @@ class Solution {
             if (p.x > x) x = p.x
             if (p.y > y) y = p.y
         }
+
         return Point(x, y)
     }
 
+
     fun solution(line: Array<IntArray>): Array<String> {
         val points = mutableListOf<Point>()
-
-        line.indices.forEach { i ->
+        for (i in line.indices) {
             for (j in i + 1 until line.size) {
-                val intersection = intersection(
+                val result = intersection(
                     line[i][0].toLong(),
                     line[i][1].toLong(),
                     line[i][2].toLong(),
@@ -52,32 +55,23 @@ class Solution {
                     line[j][1].toLong(),
                     line[j][2].toLong(),
                 )
-                intersection?.let { points.add(intersection) }
+                result?.let { points.add(result) }
             }
         }
 
-        val minimum = getMinimumPoint(points)
-        val maximum = getMaximumPoint(points)
+        val minPoint = getMin(points)
+        val maxPoint = getMax(points)
 
-        val width = (maximum.x - minimum.x + 1).toInt()
-        val height = (maximum.y - minimum.y + 1).toInt()
+        val width = (maxPoint.x - minPoint.x + 1).toInt()
+        val height = (maxPoint.y - minPoint.y + 1).toInt()
 
-        val array = Array(height) { CharArray(width) }
-        for (row in array) {
-            row.fill('.')
-        }
-
-        // 1,1 to 3,3
+        val array = Array(height) { CharArray(width) { '.' } }
         for (p in points) {
-            val x = (p.x - minimum.x).toInt()
-            val y = (maximum.y - p.y).toInt()
+            val x = (p.x - minPoint.x).toInt()
+            val y = (maxPoint.y - p.y).toInt()
             array[y][x] = '*'
         }
 
-        val result = Array(height) {""}
-        for (i in result.indices) {
-            result[i] = array[i].joinToString("")
-        }
-        return result
+        return array.map { it.joinToString("") }.toTypedArray()
     }
 }
