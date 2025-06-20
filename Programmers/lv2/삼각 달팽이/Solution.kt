2bc795b16e6d575775b5500e0f9befffa4ce61f2
Jisu Kim <kjisubit@gmail.com>
2025-06-20 20:13:54 +0900

    chore: 솔루션 개선

--- Programmers/lv2/삼각 달팽이/Solution.kt
+++ Programmers/lv2/삼각 달팽이/Solution.kt
@@ -1,47 +1,48 @@
-// n * n 크기의 어레이 생성
-// 좌표 이동 문제 -> 변화랑 걔념 사용 -> 필요 방향은 아래, 우, 상좌
-// 1. n 을 벗어나거나 다른 숫자를 만날 시, 방향 전환
-// -- 아래 -> n 벗어나지 않도록
-// -- 우 -> n 벗어나지 않도록
-// -- 상좌 -> x, y가 각각 -1 되지 않도록
-// 2. 방향 전환 후, 이동 불가할 시
+// 삼각 달팽이
+
+// 2차원 배열 생성하고 0으로 채우기
+// 2차원 배열 범위 초과 시, 방향 전환 (하, 우, 상좌)
+// 방향 전환이 연속 2번 일어날 경우 종료
 
 class Solution {
     private val dx = intArrayOf(0, 1, -1)
     private val dy = intArrayOf(1, 0, -1)
 
-    fun solution(n: Int): IntArray {
-        val triangle = Array(n) { IntArray(n) }
+    private var v = 1
+    private var d = 0
+    private var x = 0
+    private var y = 0
 
-        var v = 1
-        var x = 0
-        var y = 0
-        var d = 0
+    fun solution(n: Int): IntArray {
+        val array = Array(n) { IntArray(n) { 0 } }
+        var blockCounter = 0
 
         while (true) {
-            triangle[y][x] = v++
-            var nx = x + dx[d]
-            var ny = y + dy[d]
+            if (blockCounter == 2) break
+            array[y][x] = v
+            val tempX = x + dx[d]
+            val tempY = y + dy[d]
 
-            if (ny == n || ny == -1 || nx == n || nx == -1 || triangle[ny][nx] != 0) {
+            // 배열 크기 초과, 혹은 이미 값이 존재할 경우 방향 전환)
+            if (tempY == n || tempY < 0 || tempX == n || tempX < 0 || array[tempY][tempX] > 0) {
+                blockCounter++
                 d = (d + 1) % 3
-                nx = x + dx[d]
-                ny = y + dy[d]
-
-                if (ny == n || ny == -1 || nx == n || nx == -1 || triangle[ny][nx] != 0) break
-            }
-
-            x = nx
-            y = ny
-        }
-
-        val result = IntArray(v - 1)
-        var index = 0
-        for (j in 0 until n) {
-            for (i in 0..j) {
-                result[index++] = triangle[j][i]
+            } else {
+                blockCounter = 0
+                x = tempX
+                y = tempY
+                v++
             }
         }
-        return result
+
+        val numList = mutableListOf<Int>()
+        for (i in 0 until n) {
+            for (j in 0 until i + 1) {
+                val num = array[i][j]
+                numList.add(num)
+            }
+        }
+
+        return numList.toIntArray()
     }
 }
