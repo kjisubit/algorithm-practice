8df9538e6e3df7b9ca73f999a437d0ecc088f77b
Jisu Kim <kjisubit@gmail.com>
2025-06-30 21:53:23 +0900

    chore: 솔루션 개선

--- Programmers/lv2/문자열 압축/Solution.kt
+++ Programmers/lv2/문자열 압축/Solution.kt
@@ -1,52 +1,43 @@
-// (subString()을 사용하면 쉽게 풀리는 문제임에 유의)
+// [문자열 압축]
 
-// 1 ~ s.length 사이를 반복하여 압축 시도하기 위해 문자열 쪼개기 함수 생성
-// 매 압축 마다 다음 로직 시도하기 위해 압축 함수 생성
-// 타겟 토큰이 이전 토큰과 동일하지 않을 경우 -> 문자열에 숫자 및 이전 토큰 추가, 카운트 초기화
-// 타겟 토큰이 이전 토큰과 동일할 경우 -> 카운트 추가
+// 메서드 1 - 문자열 쪼개기 메서드
+// 메서드 2 - 문자열 압축 메서드
 
 class Solution {
     fun solution(s: String): Int {
-        var min = Int.MAX_VALUE
-        for (length in 1..s.length) {
-            val tokens = split(s, length)
-            val compressed = compress(tokens)
-            if (compressed < min) min = compressed
+        var length = s.length
+        for (i in 1..s.length) {
+            val compressed = compress(s, i)
+            if (compressed < length) length = compressed
         }
-        return min
+        return length
     }
 
-    private fun split(source: String, length: Int): List<String> {
-        val tokens = mutableListOf<String>()
-        var startIndex = 0
-        while (startIndex < source.length) {
-            var endIndex = startIndex + length
-            if (endIndex > source.length) endIndex = source.length
-            tokens.add(source.substring(startIndex, endIndex))
-            startIndex += length
-        }
-        return tokens
+    private fun split(s: String, length: Int): List<String> {
+        return s.chunked(length)
     }
 
-    private fun compress(tokens: List<String>): Int {
-        val builder = StringBuilder()
+    private fun compress(s: String, length: Int): Int {
+        val sb = StringBuilder()
 
         var last = ""
         var count = 0
-        for (token in tokens) {
-            if (token == last) {
+
+        for (token in split(s, length)) {
+            if (last == token) {
                 count++
             } else {
-                if (count > 1) builder.append(count)
-                builder.append(last)
-                last = token
+                if (count > 1) sb.append(count)
+                sb.append(last)
+
                 count = 1
+                last = token
             }
         }
 
-        if (count > 1) builder.append(count)
-        builder.append(last)
+        if (count > 1) sb.append(count)
+        sb.append(last)
 
-        return builder.length
+        return sb.length
     }
 }
