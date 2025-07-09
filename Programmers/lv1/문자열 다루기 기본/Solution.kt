1eff653a3875ed39f1022731d505db629cc49d39
Jisu Kim <kjisubit@gmail.com>
2025-07-09 20:53:58 +0900

    chore: 솔루션 개선

--- Programmers/lv1/문자열 다루기 기본/Solution.kt
+++ Programmers/lv1/문자열 다루기 기본/Solution.kt
@@ -1,5 +1,7 @@
+// [문자열 다루기 기본]
+
 class Solution {
     fun solution(s: String): Boolean {
-        return s.matches(Regex("[0-9]{4} | [0-9]{6}"))
+        return s.matches(Regex("[0-9]{4}|[0-9]{6}"))
     }
 }
