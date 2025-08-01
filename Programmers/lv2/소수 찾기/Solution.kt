046b5df1bee9215fae5d9ce81cf26f2b292a7b0d
Jisu Kim <kjisubit@gmail.com>
2025-08-01 22:08:53 +0900

    chore: 솔루션 개선

--- Programmers/lv2/소수 찾기/Solution.kt
+++ Programmers/lv2/소수 찾기/Solution.kt
@@ -1,26 +1,16 @@
-// 주어진 숫자로 생성 가능한 모든 숫자 -> 재귀 사용
-// 종료 조건 -> 더이상 사용 가능한 숫자 없음 -> 이것만 가지고 점화식 유추 불가
-// 숫자 생성 패턴 -> 1로 시작하여 조합 가능한 경우의 수 -> 1 + 3... or 1 + 5...
-// 큰 부분 문제를 작은 부분 문제로 표현할 수 있는가?
-// 1로 시작하는 경우의 수는, 13과 15로 시작하는 경우의 수(135, 153)를 모두 합한 것이다.
-// 상태: (acc, remains): acc 로 시작해 조합 가능한 모든 숫자
-// 종료: remains 의 크기가 0
-// 점화식: (acc, remains) = sigma [x] from [1] to [n] of (acc + x, remains - x)
-// 단, remains 를 매번 재귀의 input 으로 넣어줄 시, remains 의 복사한 후 원소를 지우는 과정이 들어가면서 O(n)의 시간이 추가되므로, 사용 여부 플래그를 별도의 어레이에 담도록
+// [소수 찾기]
 
-// 주어진 숫자 쪼개기
-// 재귀 함수 생성한 결과 리스트로 반환
-// 반환된 리스트의 각 원소가 소수인지 판별 (소수 여부 확인 함수 필요)
+// 재귀를 활용하여 주어진 인풋으로 생성 가능한 모든 수 생성
+// 규칙성: 92 + 1, 8, 3
+// 상태: (numbers, remains)
+// 종료: 더 이상 선택 가능한 입력값 없을 때
+// 점화식: (numbers) =  시그마 numbers first -> n (numbers + x, remains - x)
+// numbers 가 너무 많아지면, 이를 순회하는데 시간 복잡도 n 이 추가되므로 사용 여부 자체는 별도의 플레그 어레이로 관리하도록
+
+// 생성한 수는 Set 에 저장!
+// 어레이의 각 원소가 소수인지 여부 확인
 
 class Solution {
-    fun solution(nums: String): Int {
-        val numbers = nums.map { it.digitToInt() }.toIntArray()
-        val primes = mutableSetOf<Int>()
-        val isUsed = BooleanArray(numbers.size)
-        getPrimes(0, numbers, isUsed, primes)
-        return primes.size
-    }
-
     private fun isPrime(n: Int): Boolean {
         if (n <= 1) return false
         var i = 2
@@ -31,6 +21,7 @@ class Solution {
         return true
     }
 
+    // 가독성을 위해 number -> acc, remains -> numbers 로 변경하겠음
     private fun getPrimes(acc: Int, numbers: IntArray, isUsed: BooleanArray, primes: MutableSet<Int>) {
         if (isPrime(acc)) primes.add(acc)
 
@@ -42,5 +33,12 @@ class Solution {
             getPrimes(nextAcc, numbers, isUsed, primes)
             isUsed[i] = false
         }
+    }
+
+    fun solution(nums: String): Int {
+        val primes = mutableSetOf<Int>()
+        val numbers = nums.map { it.digitToInt() }.toIntArray()
+        getPrimes(0, numbers, BooleanArray(numbers.size), primes)
+        return primes.size
     }
 }
