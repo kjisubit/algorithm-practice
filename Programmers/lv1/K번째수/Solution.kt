01690d0b345dff835a414b8128f48ee24fbde18f
Jisu Kim <kjisubit@gmail.com>
2025-08-04 21:25:25 +0900

    chore: 솔루션 개선

--- Programmers/lv1/K번째수/Solution.kt
+++ Programmers/lv1/K번째수/Solution.kt
@@ -1,12 +1,18 @@
+// [k번째 수]
+
+// 시작, 종료, 타겟 인덱스 추출
+// slice 처리 후, 타겟 인덱스가 가리키는 수 가져오기
+
 class Solution {
     fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
         val answer = IntArray(commands.size)
-
         commands.forEachIndexed { i, command ->
-            val from = command[0] - 1
-            val to = command[1] - 1
-            val position = command[2] - 1
-            val target = array.slice(from..to).sorted()[position]
+            val startIndex = command[0] - 1
+            val endIndex = command[1] - 1
+            val targetIndex = command[2] - 1
+
+            val sliced = array.slice(startIndex..endIndex).sorted()
+            val target = sliced[targetIndex]
             answer[i] = target
         }
         return answer
