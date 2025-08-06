8638535601bad698a7b3f7bee14a0cb03dc4c737
Jisu Kim <kjisubit@gmail.com>
2025-08-06 21:56:19 +0900

    chore: 솔루션 개선

--- Programmers/lv1/두 개 뽑아서 더하기/Solution.kt
+++ Programmers/lv1/두 개 뽑아서 더하기/Solution.kt
@@ -1,14 +1,17 @@
-// 모든 인덱스를 더한 후, Set 에 저장
-// 이후 sort
+// [두 개 뽑아서 더하기]
+
+// 모든 경우의 수를 Set에 저장
+// 이후 정렬
 
 class Solution {
     fun solution(numbers: IntArray): IntArray {
         val set = mutableSetOf<Int>()
-        for (i in numbers.indices) {
+        for (i in 0 until numbers.size) {
             for (j in i + 1 until numbers.size) {
-                set.add(numbers[i] + numbers[j])
+                set.add (numbers[i] + numbers[j])
             }
         }
-        return set.sorted().toIntArray()
+        val answer = set.sorted().toIntArray()
+        return answer
     }
 }
