9469cfc5d062a132bcedc16cf25343f628b85021
Jisu Kim <kjisubit@gmail.com>
2025-08-21 20:49:36 +0900

    chore: 솔루션 개선

--- Programmers/lv2/피보나치 수/Solution.kt
+++ Programmers/lv2/피보나치 수/Solution.kt
@@ -1,22 +1,26 @@
+// [피보나치 수]
+
+// 피보나치 연산 결과가 매우 크기 때문에 모듈러 연산 필요
+// 반복적인 부분 문제 연산을 회피하기 위해 메모이제이션 필요
+// 재귀 fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
+
 class Solution {
     private val mem = IntArray(100001) { -1 }
 
     fun solution(n: Int): Int {
         for (i in 0..n) {
-            fibonacci(i)
+            fibonacci(n)
         }
         return fibonacci(n)
     }
 
     private fun fibonacci(n: Int): Int {
-        if (mem[n] == -1) return mem[n]
-        if (n == 0 || n == 1) return n
+        val mod = 1234567
 
-        // 값이 지나치게 커질 수 있어 나머지 연산을 수행하도록 한 것
-        // 모듈러 연산의 성질 적용 (A + B) % C = ( ( A % C ) + ( B % C) ) % C
-        // f(n) % x = (f(n-1) % x) + (f(n-2) % x) % x
-        // 여기서 f(n-1)과 f(n-2)는 내부적으로 이미 x로 나뉘어진 상태
-        mem[n] = (fibonacci(n - 1) + fibonacci(n - 2)) % 1234567
+        if (mem[n] != -1) return mem[n]
+        if (n == 0 || n == 1) return n % mod
+
+        mem[n] = (fibonacci(n - 1) + fibonacci(n - 2)) % mod
         return mem[n]
     }
 }
