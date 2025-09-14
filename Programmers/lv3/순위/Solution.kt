8a48db5bb02f64f1023d7223352601b06db19bae
Jisu Kim <kjisubit@gmail.com>
2025-09-14 21:30:39 +0900

    chore: 솔루션 개선

--- Programmers/lv3/순위/Solution.kt
+++ Programmers/lv3/순위/Solution.kt
@@ -1,48 +1,54 @@
-//   0 1 2
-// 0 F T F
-// 1 F F F
-// 2 F F F
+// [순위]
+
+// 순위 파악이 가능한 선수는 파악 가능한 싸움 결과(승리 + 패배)의 총 합이 n - 1인 경우
+// 인접 행렬을 탐색하여 승리, 패배의 수 파악
+// 승리는 u -> v -> z 순으로 파악하고, 패배는 역순인 u -> t -> s으로 카운트
 
 class Solution {
-    private fun countForward(u: Int, graph: Array<BooleanArray>, isVisited: BooleanArray): Int {
-        var count = 1
-
-        for (v in 0 until graph[u].size) {
-            if (!graph[u][v] || isVisited[v]) continue
-            isVisited[v] = true
-            count += countForward(v, graph, isVisited)
-        }
-        return count
-    }
-
-    private fun countBackward(u: Int, graph: Array<BooleanArray>, isVisited: BooleanArray): Int {
-        var count = 1
-
-        for (v in 0 until graph.size) {
-            if (!graph[v][u] || isVisited[v]) continue
-            isVisited[v] = true
-            count += countBackward(v, graph, isVisited)
-        }
-        return count
-    }
-
     fun solution(n: Int, results: Array<IntArray>): Int {
         val graph = Array(n) { BooleanArray(n) }
-
-        for (edge in results) {
-            val u = edge[0] - 1
-            val v = edge[1] - 1
+        for (result in results) {
+            val u = result[0] - 1
+            val v = result[1] - 1
             graph[u][v] = true
         }
 
-        var count = 0
+        var total = 0
         for (u in 0 until n) {
-            val wins = countForward(u, graph, BooleanArray(n)) - 1
-            val loses = countBackward(u, graph, BooleanArray(n)) - 1
-            if (wins + loses + 1 == n) {
+            val winCount = countWin(u, graph, BooleanArray(n))
+            val loseCount = countLose(u, graph, BooleanArray(n))
+            if (winCount + loseCount == n - 1) total++
+        }
+        return total
+    }
+
+    private fun countWin(u: Int, graph: Array<BooleanArray>, isVisited: BooleanArray): Int {
+        var count = 0
+        for (v in 0 until graph[u].size) {
+            if (!graph[u][v] || isVisited[v]) continue
+
+            if (graph[u][v]) {
                 count++
+                count += countWin(v, graph, isVisited)
+                isVisited[v] = true
             }
         }
+
+        return count
+    }
+
+    private fun countLose(u: Int, graph: Array<BooleanArray>, isVisited: BooleanArray): Int {
+        var count = 0
+        for (v in 0 until graph.size) {
+            if (!graph[v][u] || isVisited[v]) continue
+
+            if (graph[v][u]) {
+                count++
+                count += countLose(v, graph, isVisited)
+                isVisited[v] = true
+            }
+        }
+
         return count
     }
 }
