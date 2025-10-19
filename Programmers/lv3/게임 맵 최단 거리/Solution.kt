1124638e2c261aec9b503612d3641773bc1795e9
Jisu Kim <kjisubit@gmail.com>
2025-10-19 22:29:20 +0900

    chore: 솔루션 개선

--- Programmers/lv3/게임 맵 최단 거리/Solution.kt
+++ Programmers/lv3/게임 맵 최단 거리/Solution.kt
@@ -1,40 +1,42 @@
 // [게임 맵 최단거리]
 
-// 최단 거리 찾기 문제 -> BFS
-// 2차원 배열로 좌표 이동 구현 -> dx, dy
-// 좌표 이동 시 방문 지점, 범위 체크, 벽 존재 여부 체크
+// 최단 거리 찾기 문제 -> BFS -> 큐 사용
+// 탐색 가능한 가장 가까운 상태를 순차적으로 큐에 저장
+
+// 좌표 이동 구현 -> dx, dy
+
+// 좌표 이동 시 범위 체크, 과거 방문 이력 체크, 벽 존재 여부 체크
 
 class Solution {
-    private class State(val x: Int, val y: Int, val step: Int)
+    private class State(val x: Int, val y: Int, val steps: Int)
 
-    private val dx = arrayOf<Int>(0, 0, -1, 1)
-    private val dy = arrayOf<Int>(-1, 1, 0, 0)
+    private val dx = intArrayOf(0, 0, -1, 1)
+    private val dy = intArrayOf(-1, 1, 0, 0)
 
     fun solution(maps: Array<IntArray>): Int {
+        val q = ArrayDeque<State>()
         val isVisited = Array(maps.size) { BooleanArray(maps[0].size) }
 
-        val ad = ArrayDeque<State>()
-        ad.addLast(State(0, 0, 1))
+        q.addLast(State(0, 0, 1))
         isVisited[0][0] = true
 
-        while (ad.isNotEmpty()) {
-            val state = ad.removeFirst()
+        while (q.isNotEmpty()) {
+            val state = q.removeFirst()
 
-            if (state.y == maps.size - 1 && state.x == maps[state.y].size - 1) return state.step
+            if (state.y == maps.size - 1 && state.x == maps[0].size - 1) return state.steps
 
             for (d in 0..3) {
                 val ny = state.y + dy[d]
                 val nx = state.x + dx[d]
 
-                if (ny < 0 || ny >= maps.size || nx < 0 || nx >= maps[ny].size) continue
+                if (ny < 0 || ny >= maps.size || nx < 0 || nx >= maps[0].size) continue
 
                 if (isVisited[ny][nx]) continue
 
                 if (maps[ny][nx] == 0) continue
 
+                q.addLast(State(nx, ny, state.steps + 1))
                 isVisited[ny][nx] = true
-
-                ad.addLast(State(nx, ny, state.step + 1))
             }
         }
 
