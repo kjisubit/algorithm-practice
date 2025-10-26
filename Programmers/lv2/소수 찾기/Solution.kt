906cc5b073d5e6897d8ccd0d83a948ffd14876a2
Jisu Kim <kjisubit@gmail.com>
2025-10-26 19:12:55 +0900

    chore: 솔루션 개선

--- Programmers/lv2/소수 찾기/Solution.kt
+++ Programmers/lv2/소수 찾기/Solution.kt
@@ -1,44 +1,41 @@
 // [소수 찾기]
 
-// 재귀를 활용하여 주어진 인풋으로 생성 가능한 모든 수 생성
-// 규칙성: 92 + 1, 8, 3
-// 상태: (numbers, remains)
-// 종료: 더 이상 선택 가능한 입력값 없을 때
-// 점화식: (numbers) =  시그마 numbers first -> n (numbers + x, remains - x)
-// numbers 가 너무 많아지면, 이를 순회하는데 시간 복잡도 n 이 추가되므로 사용 여부 자체는 별도의 플레그 어레이로 관리하도록
+// method 1 - 소인수 여부 체크
 
-// 생성한 수는 Set 에 저장!
-// 어레이의 각 원소가 소수인지 여부 확인
+// method 2 - 재귀로 모든 케이스 생성
+// acc로 시작해서 만들 수 있는 모든 숫자
+// (acc, remains) = acc + (acc + a, remains - a) + (acc + b, remains - b) + ...
 
-class Solution {
+class Solution021 {
+    fun solution(nums: String): Int {
+        val primes = mutableSetOf<Int>()
+        val remains = nums.map { it.digitToInt() }.toIntArray()
+        val isUsed = BooleanArray(nums.length)
+        getPrimes(0, remains, isUsed, primes)
+
+        return primes.size
+    }
+
     private fun isPrime(n: Int): Boolean {
         if (n <= 1) return false
         var i = 2
         while (i * i <= n) {
-            if (n % i == 0) return false
+            if(n % i == 0) return false
             i++
         }
         return true
     }
 
-    // 가독성을 위해 number -> acc, remains -> numbers 로 변경하겠음
-    private fun getPrimes(acc: Int, numbers: IntArray, isUsed: BooleanArray, primes: MutableSet<Int>) {
+    private fun getPrimes(acc: Int, remains: IntArray, isUsed: BooleanArray, primes: MutableSet<Int>) {
         if (isPrime(acc)) primes.add(acc)
 
-        for (i in numbers.indices) {
+        for (i in 0 until remains.size) {
             if (isUsed[i]) continue
-            val nextAcc = acc * 10 + numbers[i]
 
             isUsed[i] = true
-            getPrimes(nextAcc, numbers, isUsed, primes)
+            val newAcc = (acc * 10) + remains[i]
+            getPrimes(newAcc, remains, isUsed, primes)
             isUsed[i] = false
         }
-    }
-
-    fun solution(nums: String): Int {
-        val primes = mutableSetOf<Int>()
-        val numbers = nums.map { it.digitToInt() }.toIntArray()
-        getPrimes(0, numbers, BooleanArray(numbers.size), primes)
-        return primes.size
     }
 }
