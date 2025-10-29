bb253dc5cf8115bb6d1583db906c3a55d72e951e
Jisu Kim <kjisubit@gmail.com>
2025-10-29 20:42:53 +0900

    chore: 솔루션 개선

--- Programmers/lv2/쿼드압축 후 세기/Solution.kt
+++ Programmers/lv2/쿼드압축 후 세기/Solution.kt
@@ -1,45 +1,43 @@
 // [쿼드압축 후 개수 세기]
 
-// 재귀
-// 0, 1의 개수는 Count 객체로 전달
-// 사각형을 최대한 쪼갠 후, 부분 문제 누적 결과 전달하기
+// 분할 반복 -> 재귀
 
+// [class Count]
+// property -> zeros, ones
+// method -> add
+
+// [method count]
 // 점화식
-// (y, x, size) =
-// (y, x, size/2) +
-// (y, x + size/2, size/2) +
-// (y + size/2, x, size/2) +
-// (y + size/2, x + size/2, size/2)
-
-// 종료 조건
-// 부분 문제의 사각형이 모두 한 개의 숫자로 채워진 경우
+// (offsetY, offsetX, l) =
+// (offsetY, offsetX, l/2)+ (offsetY, offsetX + l/2, l/2) + (offsetY + l/2, offsetX, l/2) + (offsetY + l/2, offsetX + l/2, l/2)
+// 종료 조건 -> 범위 내 모든 숫자가 동일한 경우
 
 class Solution {
     private class Count(val zeros: Int, val ones: Int) {
-        fun add(other: Count): Count {
-            return Count(zeros + other.zeros, ones + other.ones)
+        fun add(count: Count): Count {
+            return Count(zeros + count.zeros, ones + count.ones)
         }
     }
 
-    private fun count(arr: Array<IntArray>, y: Int, x: Int, size: Int): Count {
-        val h = size / 2
-        for (j in y until y + size) {
-            for (i in x until x + size) {
-                if (arr[j][i] != arr[y][x]) {
-                    return count(arr, y, x, h)
-                        .add(count(arr, y, x + h, h))
-                        .add(count(arr, y + h, x, h))
-                        .add(count(arr, y + h, x + h, h))
+    private fun count(offsetY: Int, offsetX: Int, length: Int, arr: Array<IntArray>): Count {
+        val newLength = length / 2
+        for (y in offsetY until offsetY + length) {
+            for (x in offsetX until offsetX + length) {
+                if (arr[y][x] != arr[offsetY][offsetX]) {
+                    return count(offsetY, offsetX, newLength, arr)
+                        .add(count(offsetY, offsetX + newLength, newLength, arr))
+                        .add(count(offsetY + newLength, offsetX, newLength, arr))
+                        .add(count(offsetY + newLength, offsetX + newLength, newLength, arr))
                 }
             }
         }
 
-        return if (arr[y][x] == 0) Count(1, 0)
-        else Count(0, 1)
+        if (arr[offsetY][offsetX] == 0) return Count(1, 0)
+        else return Count(0, 1)
     }
 
     fun solution(arr: Array<IntArray>): IntArray {
-        val count = count(arr, 0, 0, arr.size)
+        val count = count(0, 0, arr.size, arr)
         return intArrayOf(count.zeros, count.ones)
     }
 }
