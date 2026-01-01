abf9adf965f52f499d2a16da2ad6c39256a86b11
Jisu Kim <kjisubit@gmail.com>
2026-01-01 23:10:04 +0900

    솔루션 개선

--- Programmers/lv1/숫자 문자열과 영단어/Solution.kt
+++ Programmers/lv1/숫자 문자열과 영단어/Solution.kt
@@ -1,13 +1,13 @@
 // [숫자 문자열과 영단어]
 
 class Solution {
-    val array = arrayOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    private val array = arrayOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
 
     fun solution(s: String): Int {
-        var string = s
-        array.forEachIndexed { i, value ->
-            string = string.replace(value, i.toString())
+        var answer = s
+        array.forEachIndexed { i, num ->
+            answer = answer.replace(num, i.toString())
         }
-        return string.toInt()
+        return answer.toInt()
     }
 }
