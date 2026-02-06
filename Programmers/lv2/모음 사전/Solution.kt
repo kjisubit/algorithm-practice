e357208839c4e376c986254f3478104b7ec4f7c0
Jisu Kim <kjisubit@gmail.com>
2026-02-06 20:22:17 +0900

    chore: 솔루션 개선

--- Programmers/lv2/모음 사전/Solution.kt
+++ Programmers/lv2/모음 사전/Solution.kt
@@ -1,31 +1,32 @@
 // [모음 사전]
 
-// 재귀로 모든 경우의 수 만들어 사전 만들기
-// a, aa, aaa, aaaa, aaaaa, aaaai, aaaae, aaaao, aaaau
-// 해당 단어가 몇 번째 인덱스인지 확인하여 리턴
+// 딕셔너리 생성
+// 입력값의 인덱스 위치 리턴
 
-// 점화식 dfs
-// (word) = word + (word + 'a') + (word + 'e') + (word + 'i') + (word + 'o') + (word + 'u')
+// 점화식
+// (word) = (word) + (word + A) + (word + E) + (word + I) ...
+// 구조상 (word)의 사전 저장이 필수
 
 // 종료 조건
-// 단어 길이가 5
+// word 길이 == 5
 
 class Solution {
     private val chars = arrayOf('A', 'E', 'I', 'O', 'U')
 
-    fun solution(word: String): Int {
-        val dict = mutableListOf<String>()
-        makeWord("", dict)
-        return dict.indexOf(word) + 1
-    }
+    private fun generate(word: String, dict: MutableList<String>) {
+        if (word.length > 5) return
 
-    private fun makeWord(word: String, dict: MutableList<String>) {
         if (word.isNotEmpty()) dict.add(word)
 
-        if (word.length == 5) return
-
-        for (c in chars) {
-            makeWord(word + c, dict)
+        for (char in chars) {
+            generate(word + char, dict)
         }
+    }
+
+    fun solution(word: String): Int {
+        val dict = mutableListOf<String>()
+        generate("", dict)
+
+        return dict.indexOf(word) + 1
     }
 }
