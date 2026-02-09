d2aad42f45105450ede5301f6256378f49b28f75
Jisu Kim <kjisubit@gmail.com>
2026-02-09 22:26:22 +0900

    chore: 솔루션 개선

--- Programmers/lv3/단어 변환/Solution.kt
+++ Programmers/lv3/단어 변환/Solution.kt
@@ -1,45 +1,51 @@
 // [단어 변환]
 
-// 최단 과정 -> BFS
+// 최단 과정 찾기 -> BFS -> Queue
 
-// 클래스: 상태 나타내기용
-// step(변환 횟수), word(단어)
+// words에 convert 가능한 단어 존재할 경우
+// - count+
+// - 방문 체크
+// - q에 추가
 
-// 메서드: 단어 변환 가능 여부 확인
-// 문자가 한 개만 차이나는 경우 변환 가능
+// 메서드
+// - 알파벳 한 글자 차이 여부 확인
+
+// 클래스
+// - 멤버: 단어, 변환 횟수
 
 class Solution {
     private class State(val word: String, val step: Int)
 
-    private fun isConvertible(start: String, dst: String): Boolean {
+    private fun convertible(original: String, comparison: String): Boolean {
         var count = 0
-        for (i in start.indices) {
-            if (start[i] != dst[i]) count++
+        for (i in original.indices) {
+            if (original[i] != comparison[i]) count++
         }
-
         return count == 1
     }
 
     fun solution(begin: String, target: String, words: Array<String>): Int {
+        val q = ArrayDeque<State>()
+        val state = State(begin, 0)
+        q.addLast(state)
+
         val isVisited = BooleanArray(words.size)
 
-        val ad = ArrayDeque<State>()
-        ad.addFirst(State(begin, 0))
-
-        while (ad.isNotEmpty()) {
-            val state = ad.removeFirst()
-
+        while (q.isNotEmpty()) {
+            val state = q.removeFirst()
             if (state.word == target) return state.step
 
+            // 자식 하나로부터 여러 개의 자식 생성
             for (i in words.indices) {
-                val next = words[i]
-
-                if (!isConvertible(state.word, next)) continue
+                val word = words[i]
+                val isConvertible = convertible(state.word, word)
+                if (!isConvertible) continue
 
                 if (isVisited[i]) continue
                 isVisited[i] = true
 
-                ad.addLast(State(next, state.step + 1))
+                val newState = State(word, state.step + 1)
+                q.addLast(newState)
             }
         }
 
