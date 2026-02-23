4d306f762cd32ebaa264a381503e23f25ad6049d
Jisu Kim <kjisubit@gmail.com>
2026-02-23 20:05:24 +0900

    chore: 솔루션 개선

--- Programmers/lv2/전화번호 목록/Solution.kt
+++ Programmers/lv2/전화번호 목록/Solution.kt
@@ -1,25 +1,24 @@
 // [전화번호 목록]
 
-// 원소 순회 시, 시간 복잡도 초과
-// -> 전처리 필요
+// 모든 원소 비교 시, 시간 복잡도 1억 이상
 
-// 모든 prefix Set에 저장
-// -> 단, 문자열 스스로가 자신의 접두어가 되지 않도록 length-1 까지만 저장
+// 가능한 모든 prefix 생성
 
 // prefix 존재 여부 확인
 
 class Solution {
-    fun solution(phoneBook: Array<String>): Boolean {
-        val prefixes = mutableSetOf<String>()
+    fun solution(phone_book: Array<String>): Boolean {
+        val prefixes = mutableListOf<String>()
 
-        for (phone in phoneBook) {
-            for (i in 1 until phone.length) {
-                prefixes.add(phone.substring(0, i))
+        phone_book.forEach { phoneNumber ->
+            for (i in 1 until phoneNumber.length) {
+                val prefix = phoneNumber.substring(0, i)
+                prefixes.add(prefix)
             }
         }
 
-        for (phone in phoneBook) {
-            if (prefixes.contains(phone)) return false
+        phone_book.forEach { phoneNumber ->
+            if (prefixes.contains(phoneNumber)) return false
         }
 
         return true
