be4bdb42e2caddfb5a59c07963dcfdf92d428c23
Jisu Kim <kjisubit@gmail.com>
2026-03-01 23:02:03 +0900

    chore: 솔루션 개선

--- Programmers/lv2/교점에 별 만들기/Solution.kt
+++ Programmers/lv2/교점에 별 만들기/Solution.kt
@@ -1,77 +1,88 @@
-// 좌표는 클래스로 표현
-// 메서드 1 -> 교차점 리턴
-// 메서드 2 -> 최소 좌표 리턴
-// 메서드 3 -> 최대 좌표 리턴
-// 마지막에 사각형 리턴
+// 클래스 1
+// -- 좌표 표현
+// -- 멤버 (x, y)
+
+// 메서드 1
+// -- 교점 만들기
+// -- line의 모든 어레이 비교
+
+// 메서드 2
+// -- 최소 좌표 리턴
+
+// 메서드 3
+// -- 최대 좌표 리턴
+
+// 메서드 4
+// -- 문자열로 좌표 표현하기
+// -- x는 그대로 사용, y는 상하 반전 필요
 
 class Solution {
     private class Point(val x: Long, val y: Long)
 
-    // 교차점 리턴
-    private fun intersection(a: Long, b: Long, e: Long, c: Long, d: Long, f: Long): Point? {
-        val x = (b * f - e * d).toDouble() / (a * d - b * c)
-        val y = (e * c - a * f).toDouble() / (a * d - b * c)
-
-        if (x % 1 == 0.0 && y % 1 == 0.0) return Point(x.toLong(), y.toLong())
-        return null
-    }
-
-    // 최소 좌표 리턴
-    private fun getMin(points: List<Point>): Point {
-        var x = Long.MAX_VALUE
-        var y = Long.MAX_VALUE
-
-        for (p in points) {
-            if (p.x < x) x = p.x
-            if (p.y < y) y = p.y
-        }
-
-        return Point(x, y)
-    }
-
-    // 최대 좌표 리턴
-    private fun getMax(points: List<Point>): Point {
-        var x = Long.MIN_VALUE
-        var y = Long.MIN_VALUE
-
-        for (p in points) {
-            if (p.x > x) x = p.x
-            if (p.y > y) y = p.y
-        }
-
-        return Point(x, y)
-    }
-
-
     fun solution(line: Array<IntArray>): Array<String> {
         val points = mutableListOf<Point>()
-        for (i in line.indices) {
+        for (i in 0 until line.size) {
             for (j in i + 1 until line.size) {
-                val result = intersection(
+                val point = getIntersection(
                     line[i][0].toLong(),
                     line[i][1].toLong(),
                     line[i][2].toLong(),
                     line[j][0].toLong(),
                     line[j][1].toLong(),
-                    line[j][2].toLong(),
+                    line[j][2].toLong()
                 )
-                result?.let { points.add(result) }
+                point?.let { points.add(point) }
             }
         }
 
-        val minPoint = getMin(points)
-        val maxPoint = getMax(points)
+        val minPoint = getMinPoint(points)
+        val maxPoint = getMaxPoint(points)
 
+        return buildArray(minPoint, maxPoint, points)
+    }
+
+    private fun getIntersection(a: Long, b: Long, e: Long, c: Long, d: Long, f: Long): Point? {
+        val x = (b * f - e * d).toDouble() / (a * d - b * c)
+        val y = (e * c - a * f).toDouble() / (a * d - b * c)
+        if (x % 1 == 0.0 && y % 1 == 0.0) return Point(x.toLong(), y.toLong())
+        return null
+    }
+
+    private fun getMinPoint(points: List<Point>): Point {
+        var minX = Long.MAX_VALUE
+        var minY = Long.MAX_VALUE
+
+        points.forEach {
+            if (it.x < minX) minX = it.x
+            if (it.y < minY) minY = it.y
+        }
+
+        return Point(minX, minY)
+    }
+
+    private fun getMaxPoint(points: List<Point>): Point {
+        var maxX = Long.MIN_VALUE
+        var maxY = Long.MIN_VALUE
+
+        points.forEach {
+            if (it.x > maxX) maxX = it.x
+            if (it.y > maxY) maxY = it.y
+        }
+
+        return Point(maxX, maxY)
+    }
+
+    private fun buildArray(minPoint: Point, maxPoint: Point, points: List<Point>): Array<String> {
         val width = (maxPoint.x - minPoint.x + 1).toInt()
         val height = (maxPoint.y - minPoint.y + 1).toInt()
 
-        val array = Array(height) { CharArray(width) { '.' } }
-        for (p in points) {
-            val x = (p.x - minPoint.x).toInt()
-            val y = (maxPoint.y - p.y).toInt()
-            array[y][x] = '*'
+        val board = Array<CharArray>(height) { CharArray(width) { '.' } }
+        points.forEach {
+            val x = (it.x - minPoint.x).toInt()
+            val y = (maxPoint.y - it.y).toInt()
+            board[y][x] = '*'
         }
 
-        return array.map { it.joinToString("") }.toTypedArray()
+        return board.map { it.joinToString("") }.toTypedArray()
     }
 }
