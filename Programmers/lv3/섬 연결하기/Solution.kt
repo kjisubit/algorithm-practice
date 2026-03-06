84ef6977aac8cda4b95b9d5dfe54d8cbc7473066
Jisu Kim <kjisubit@gmail.com>
2026-03-06 21:25:52 +0900

    chore: 솔루션 개선

--- Programmers/lv3/섬 연결하기/Solution.kt
+++ Programmers/lv3/섬 연결하기/Solution.kt
@@ -1,56 +1,59 @@
 // [섬 연결하기]
 
-// 주어진 입력으로 최소 신장 트리 구성하도록
-// 크루스칼 알고리즘 필요
+// 가중치가 최소값인 신장 트리 구성 -> 크루스칼 알고리즘
 
-class Solution053 {
-    private class Node(var depth: Int = 1, var parent: Node? = null) {
-        fun isConnected(o: Node): Boolean {
-            return root() == o.root()
+class Solution {
+    private class Node(
+        private var parent: Node? = null,
+        private var depth: Int = 1
+    ) {
+        fun isConnected(node: Node): Boolean {
+            return getRoot() == node.getRoot()
         }
 
-        fun merge(o: Node) {
-            if (isConnected(o)) return
-
-            val root1 = root()
-            val root2 = o.root()
-
-            if (root1.depth > root2.depth) {
-                root2.parent = root1
-            } else if (root1.depth < root2.depth) {
-                root1.parent = root2
-            } else {
-                root2.parent = root1;
-                root1.depth += 1
-            }
-        }
-
-        private fun root(): Node {
+        fun getRoot(): Node {
             if (parent == null) return this
-            return parent!!.root()
+            return parent!!.getRoot()
+        }
+
+        fun merge(node: Node) {
+            if (isConnected(node)) return
+
+            val root = getRoot()
+            val nodeRoot = node.getRoot()
+
+            if (root.depth > nodeRoot.depth) {
+                nodeRoot.parent = root
+            } else if (root.depth < nodeRoot.depth) {
+                root.parent = nodeRoot
+            } else {
+                nodeRoot.parent = root
+                root.depth++
+            }
         }
     }
 
     private class Edge(val u: Int, val v: Int, val cost: Int)
 
     fun solution(n: Int, costs: Array<IntArray>): Int {
-        val edges = costs.map { Edge(it[0], it[1], it[2]) }
-            .sortedWith(compareBy { it.cost }).toTypedArray()
+        val nodes = Array(n) { Node() }
 
-        val nodes = Array(n) {
-            Node()
-        }
+        val edges = costs.map { intArray ->
+            Edge(intArray[0], intArray[1], intArray[2])
+        }.sortedWith(compareBy { it.cost })
 
-        var totalCost = 0
+        var cost = 0
+
         for (edge in edges) {
             val node1 = nodes[edge.u]
             val node2 = nodes[edge.v]
 
             if (node1.isConnected(node2)) continue
+
             node1.merge(node2)
-            totalCost += edge.cost
+            cost += edge.cost
         }
 
-        return totalCost
+        return cost
     }
 }
