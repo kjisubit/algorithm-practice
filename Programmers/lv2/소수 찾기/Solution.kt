b153893cdffec440d5704e2bf41fdf62da4be09e
Jisu Kim <kjisubit@gmail.com>
2026-03-18 23:13:28 +0900

    chore: 솔루션 개선

--- Programmers/lv2/소수 찾기/Solution.kt
+++ Programmers/lv2/소수 찾기/Solution.kt
@@ -1,41 +1,50 @@
 // [소수 찾기]
 
-// method 1 - 소인수 여부 체크
+// 프로세스
+// -- 가능한 모든 경우의 수 생성 (재귀 필요)
+// -- 앞자리 0인 경우 예외처리
+// -- 소수 체크
 
-// method 2 - 재귀로 모든 케이스 생성
-// acc로 시작해서 만들 수 있는 모든 숫자
-// (acc, remains) = acc + (acc + a, remains - a) + (acc + b, remains - b) + ...
+// 재귀
+// -- 상태: (num, remains) -> num으로 시작하는, remains로 조합 가능한 케이스
+// -- 종료: remains.size == 0
+// -- 점화식: (num, remains) = num + (num + remains[0], remains - remains[0])
 
-class Solution021 {
+class Solution {
     fun solution(nums: String): Int {
-        val primes = mutableSetOf<Int>()
-        val remains = nums.map { it.digitToInt() }.toIntArray()
+        val remains = nums.map { it - '0' }.toIntArray()
         val isUsed = BooleanArray(nums.length)
-        getPrimes(0, remains, isUsed, primes)
+        val primes = mutableSetOf<Int>()
+        generateAllNumbers(0, remains, isUsed, primes)
 
         return primes.size
     }
 
-    private fun isPrime(n: Int): Boolean {
-        if (n <= 1) return false
-        var i = 2
-        while (i * i <= n) {
-            if(n % i == 0) return false
-            i++
-        }
-        return true
-    }
-
-    private fun getPrimes(acc: Int, remains: IntArray, isUsed: BooleanArray, primes: MutableSet<Int>) {
+    private fun generateAllNumbers(
+        acc: Int,
+        remains: IntArray,
+        isUsed: BooleanArray,
+        primes: MutableSet<Int>
+    ) {
         if (isPrime(acc)) primes.add(acc)
 
-        for (i in 0 until remains.size) {
+        for (i in remains.indices) {
             if (isUsed[i]) continue
 
             isUsed[i] = true
-            val newAcc = (acc * 10) + remains[i]
-            getPrimes(newAcc, remains, isUsed, primes)
+            val newAcc = acc * 10 + remains[i]
+            generateAllNumbers(newAcc, remains, isUsed, primes)
             isUsed[i] = false
         }
+    }
+
+    private fun isPrime(value: Int): Boolean {
+        if (value <= 1) return false
+        var i = 2
+        while (i * i <= value) {
+            if (value % i == 0) return false
+            i++
+        }
+        return true
     }
 }
