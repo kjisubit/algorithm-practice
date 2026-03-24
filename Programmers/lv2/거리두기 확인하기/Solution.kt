f3f08c919512018970d5fe33e008a9ceeafb2a1a
Jisu Kim <kjisubit@gmail.com>
2026-03-24 21:32:14 +0900

    chore: 솔루션 개선

--- Programmers/lv2/거리두기 확인하기/Solution.kt
+++ Programmers/lv2/거리두기 확인하기/Solution.kt
@@ -1,57 +1,54 @@
 // [거리두기 확인하기]
 
-// 각 행을 2차원 어레이로 변환
+// 좌표 이동 문제 -> 변화량 사용
 
-// 메서드 1-1 - 자리 순회
-
-// 메서드 1-2 - P의 동서남북 거리두기 확인
-// p 근처에 p가 한 개라도 있으면 x
-
-// 메서드 1-3 - O의 동서남북 거리두기 확인
-// o 근처에 p가 한 개라도 있으면 x
+// 프로세스
+// -- places 순회
+// -- 대기실 하나를 2차원 어레이로 변환
+// -- 2차원 어레이 순회
+// -- 맨해튼 거리 1 검사 -> 현재 위치가 p인 경우, 상하좌우 검사 후 p가 하나라도 있으면 실패
+// -- 맨해튼 거리 2 검사 -> 맨해튼 거리 1 위치가 o인 경우, (왔던 경로를 제외하고) 상하좌우 검사 후 p가 하나라도 있으면 실패
 
 class Solution {
     private val dx = intArrayOf(0, 0, -1, 1)
-    private val dy = intArrayOf(-1, 1, 0 ,0)
+    private val dy = intArrayOf(-1, 1, 0, 0)
 
     fun solution(places: Array<Array<String>>): IntArray {
-        val answer = IntArray(places.size)
-
-        for (i in places.indices) {
-            val room = places[i].map { it.toCharArray() }.toTypedArray()
-            if (isDistanced(room)) answer[i] = 1
-            else answer[i] = 0
+        val answer = mutableListOf<Int>()
+        for (place in places) {
+            val room = place.map { it.toCharArray() }.toTypedArray()
+            val result = isRoomDistanced(room)
+            if (result) {
+                answer.add(1)
+            } else {
+                answer.add(0)
+            }
         }
-
-        return answer
+        return answer.toIntArray()
     }
 
-    private fun isDistanced(room: Array<CharArray>): Boolean {
+    // 강의실 검사
+    private fun isRoomDistanced(room: Array<CharArray>): Boolean {
         for (y in room.indices) {
             for (x in room.indices) {
-                if (room[y][x] != 'P') continue
-                else {
-                    if (!isDistanced(room, y, x)) return false
-                }
+                val c = room[y][x]
+                if (c == 'P' && !isNearFirstDistanced(room, y, x)) return false
             }
         }
         return true
     }
 
-    private fun isDistanced(room: Array<CharArray>, y: Int, x: Int): Boolean {
+    // 맨해튼 거리 1 검사
+    private fun isNearFirstDistanced(room: Array<CharArray>, y: Int, x: Int): Boolean {
         for (d in 0..3) {
             val ny = y + dy[d]
             val nx = x + dx[d]
 
-            if (ny >= room.size || ny < 0 || nx >= room.size || nx < 0) continue
+            if (ny < 0 || ny >= room.size || nx < 0 || nx >= room.size) continue
 
-            when (room[ny][nx]) {
-                'P' -> {
-                    return false
-                }
-                'X' -> {
-                    continue
-                }
+            val c = room[ny][nx]
+            when (c) {
+                'P' -> return false
                 'O' -> {
                     val exclude = when (d) {
                         0 -> 1
@@ -59,34 +56,25 @@ class Solution {
                         2 -> 3
                         else -> 2
                     }
-
-                    if(!isDistanced(room, ny, nx, exclude)) return false
+                    if (!isNearSecondDistanced(room, ny, nx, exclude)) return false
                 }
             }
         }
-
         return true
     }
 
-    private fun isDistanced(room: Array<CharArray>, y: Int, x: Int, exclude: Int): Boolean {
+    // 맨해튼 거리 2 검사
+    private fun isNearSecondDistanced(room: Array<CharArray>, y: Int, x: Int, exclude: Int): Boolean {
         for (d in 0..3) {
+            if (d == exclude) continue
+
             val ny = y + dy[d]
             val nx = x + dx[d]
 
-            if (d == exclude) continue
+            if (ny < 0 || ny >= room.size || nx < 0 || nx >= room.size) continue
 
-            if (ny >= room.size || ny < 0 || nx >= room.size || nx < 0) continue
-            when (room[ny][nx]) {
-                'P' -> {
-                    return false
-                }
-                'X' -> {
-                    continue
-                }
-                'O' -> {
-                    continue
-                }
-            }
+            val c = room[ny][nx]
+            if (c == 'P') return false
         }
 
         return true
