9b99d490b634b7eaa498ab1a2d4e63a54cbc7693
Jisu Kim <kjisubit@gmail.com>
2026-05-03 21:16:02 +0900

    chore: 솔루션 개선

--- Programmers/lv2/소수 찾기/Solution.kt
+++ Programmers/lv2/소수 찾기/Solution.kt
@@ -1,48 +1,41 @@
 // [소수 찾기]
 
-// 프로세스
-// -- 가능한 모든 경우의 수 생성 (재귀 필요)
-// -- 앞자리 0인 경우 예외처리
-// -- 소수 체크
-
-// 재귀
-// -- 상태: (num, remains) -> num으로 시작하는, remains로 조합 가능한 케이스
+// 생성 가능한 모든 경우의 수 만들기
+// -- 완전 탐색
+// -- 상태 : (acc, remains) = acc로 시작하고, remains로 만들 수 있는 모든 경우의 수
 // -- 종료: remains.size == 0
-// -- 점화식: (num, remains) = num + (num + remains[0], remains - remains[0])
+// -- 점화식: (acc, remains) = (acc * 10 + a, remains - a)
+
+// 소수 체크
 
 class Solution {
     fun solution(nums: String): Int {
         val remains = nums.map { it - '0' }.toIntArray()
-        val isUsed = BooleanArray(nums.length)
+        val isVisited = BooleanArray(nums.length)
         val primes = mutableSetOf<Int>()
-        generateAllNumbers(0, remains, isUsed, primes)
-
+        generateAll(0, remains, isVisited, primes)
         return primes.size
     }
 
-    private fun generateAllNumbers(
-        acc: Int,
-        remains: IntArray,
-        isUsed: BooleanArray,
-        primes: MutableSet<Int>
-    ) {
+    private fun generateAll(acc: Int, remains: IntArray, isVisited: BooleanArray, primes: MutableSet<Int>) {
         if (isPrime(acc)) primes.add(acc)
 
-        for (i in remains.indices) {
-            if (isUsed[i]) continue
+        for (i in 0 until remains.size) {
+            if (isVisited[i]) continue
 
-            isUsed[i] = true
             val newAcc = acc * 10 + remains[i]
-            generateAllNumbers(newAcc, remains, isUsed, primes)
-            isUsed[i] = false
+            isVisited[i] = true
+            generateAll(newAcc, remains, isVisited, primes)
+            isVisited[i] = false
         }
     }
 
-    private fun isPrime(value: Int): Boolean {
-        if (value <= 1) return false
+    private fun isPrime(num: Int): Boolean {
+        if (num == 0 || num == 1) return false
+
         var i = 2
-        while (i * i <= value) {
-            if (value % i == 0) return false
+        while (i * i <= num) {
+            if (num % i == 0) return false
             i++
         }
         return true
