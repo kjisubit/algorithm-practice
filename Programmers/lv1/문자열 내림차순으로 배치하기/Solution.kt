8cfb3457bbdc2233c3ff4a38a3d441f12d8428c2
Jisu Kim <kjisubit@gmail.com>
2026-05-13 21:55:37 +0900

    chore: 솔루션 개선

--- Programmers/lv1/문자열 내림차순으로 배치하기/Solution.kt
+++ Programmers/lv1/문자열 내림차순으로 배치하기/Solution.kt
@@ -1,7 +1,9 @@
 // [문자열 내림차순으로 배치하기]
 
+// 정렬 메커니즘에 대한 이해가 필요한 문제 (메서드 암기 X)
+
 class Solution {
     fun solution(s: String): String {
-        return s.toList().sorted().reversed().joinToString("")
+        return s.toList().sortedWith { a, b -> b.compareTo(a) }.joinToString("")
     }
 }
