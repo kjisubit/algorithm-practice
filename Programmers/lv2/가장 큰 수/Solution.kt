a59a3a22638c27bc6edcba7be7caca6ef84b83ec
Jisu Kim <kjisubit@gmail.com>
2026-06-02 20:36:09 +0900

    chore: 솔루션 개선

--- Programmers/lv2/가장 큰 수/Solution.kt
+++ Programmers/lv2/가장 큰 수/Solution.kt
@@ -1,14 +1,14 @@
 // [가장 큰 수]
 
-// 두 수를 정방향 or 역방향으로 각각 이어붙인 후, 결과가 더 커지는 순으로 정렬 수행
+// 1. 두 수를 이어붙인 결과에 따라 정렬 수행
 
 class Solution {
     fun solution(numbers: IntArray): String {
-        val regex = Regex("^0+")
-        return numbers.map { it.toString() }.sortedWith { a, b ->
-            val ordered = (a + b).toInt()
-            val reversed = (b + a).toInt()
-            reversed - ordered
+        val regex = Regex("^0{2,}")
+        return numbers.sortedWith { a, b ->
+            val ordered = (a.toString() + b.toString()).toInt()
+            val reversed = (b.toString() + a.toString()).toInt()
+            reversed.compareTo(ordered)
         }.joinToString("").replace(regex, "0")
     }
 }
