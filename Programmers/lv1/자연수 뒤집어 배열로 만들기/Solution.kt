0ee2f9498bc99b358733de7facf98c773e975162
Jisu Kim <kjisubit@gmail.com>
2026-06-22 21:03:27 +0900

    chore: 솔루션 개선

--- Programmers/lv1/자연수 뒤집어 배열로 만들기/Solution.kt
+++ Programmers/lv1/자연수 뒤집어 배열로 만들기/Solution.kt
@@ -1,5 +1,7 @@
-class Solution {
+// 숫자 -> 문자열 -> 문자 어레이 -> 리버스 -> 정수 어레이
+
+class Solution{
     fun solution(n: Long): IntArray {
-        return n.toString().reversed().map { it -> it.digitToInt() }.toIntArray()
+        return n.toString().reversed().map { it - '0' }.toIntArray()
     }
 }
