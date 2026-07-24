23db537ed7db7f1aa1cb066c4373565b7226387b
Jisu Kim <kjisubit@gmail.com>
2026-07-24 18:46:35 +0900

    refactor: 솔루션 개선

--- Programmers/lv3/하노이의 탑/Solution.kt
+++ Programmers/lv3/하노이의 탑/Solution.kt
@@ -1,30 +1,31 @@
 // [하노이의 탑]
 
-// 반복적인 작업 필요
-// 큰 부분 문제로 작은 부분 문제 표현 가능한가?
-// n개의 원판을 옮기는 문제는 n - 1개의 원판을 옮기는 문제로 표현 가능하다.
-// 점화식: (n, from, to) = (n - 1, from, 6 - from - to) + (1, from, to) + (n - 1, 6 - from - to, to)
-// 종료조건 -> 한 개의 원판을 옮길 때 -> n = 1
+// 재귀 필요 -> 큰 부분 문제를 작은 부분 문제로 표현
 
-// 값 누적 방식
-// 큰 부분 문제는 작은 부분 문제가 먼저 선행되어야 하므로, 종료 조건의 원판 옮기는 순서가 완료될 때마다 값을 누적하도록
+// 상태
+// x에서 y로 n개의 원판 이동 -> (x, y, n)
+
+// 종료
+// 1개의 원판 이동
+
+// 점화
+// (x, y, n) = (x, 6 - x - y , n - 1) + (x, y, 1) + (6 - x - y, y, n - 1)
 
 class Solution {
     fun solution(n: Int): Array<IntArray> {
-        val order = mutableListOf<IntArray>()
-        hanoi(n, 1, 3, order)
-        return order.toTypedArray()
+        val answer = mutableListOf<IntArray>()
+        hanoi(1, 3, n, answer)
+        return answer.toTypedArray()
     }
 
-    private fun hanoi(n: Int, from: Int, to: Int, order: MutableList<IntArray>) {
+    private fun hanoi(from: Int, to: Int, n: Int, answer: MutableList<IntArray>) {
         if (n == 1) {
-            order.add(intArrayOf(from, to))
+            answer.add(intArrayOf(from, to))
             return
         }
 
-        val other = 6 - from - to
-        hanoi(n - 1, from, other, order)
-        hanoi(1, from, to, order)
-        hanoi(n - 1, other, to, order)
+        hanoi(from, 6 - from - to, n - 1, answer)
+        hanoi(from, to, 1, answer)
+        hanoi(6 - from - to, to, n - 1, answer)
     }
 }
