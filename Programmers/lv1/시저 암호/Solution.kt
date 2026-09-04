d7315eeb1efbe381aa9617561d596ba172d9dcac
Jisu Kim <kjisubit@gmail.com>
2026-09-04 20:19:19 +0900

    refactor: 솔루션 개선

--- Programmers/lv1/시저 암호/Solution.kt
+++ Programmers/lv1/시저 암호/Solution.kt
@@ -1,25 +1,30 @@
-// 1. 문자열 순회
+// [시저 암호]
 
-// 2. 대/소문자, 공백에 맞춰 push 로직 분기
+// 문자열 순회 -> 각 문자 push
 
 class Solution {
-    private fun push(c: Char, n: Int): Char {
-        if (c == ' ') return ' ' // 공백
-
-        val range = 'z' - 'a' + 1
-        val startPoint = if (c in 'a'..'z') 'a' else 'A'
-
-        val pushed = startPoint + (c.code - startPoint.code + n) % range
-        return pushed
-    }
-
     fun solution(s: String, n: Int): String {
         val sb = StringBuilder()
 
         for (c in s) {
             sb.append(push(c, n))
         }
-
         return sb.toString()
+    }
+
+    private fun push(c: Char, n: Int): Char {
+        val range = 'z' - 'a' + 1
+        if (c in 'a'..'z') {
+            val offset = c - 'a'
+            println(offset)
+            val pushedOffset = offset + n
+            return 'a' + (offset + n) % range
+        } else if (c in 'A'..'Z') {
+            val offset = c - 'A'
+            val pushedOffset = offset + n
+            return 'A' + (offset + n) % range
+        } else {
+            return ' '
+        }
     }
 }
