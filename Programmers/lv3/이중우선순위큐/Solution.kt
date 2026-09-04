bb1a3f105f9c720f066a12acbd69fb55cb2e82b4
Jisu Kim <kjisubit@gmail.com>
2026-09-04 20:20:39 +0900

    refactor: 솔루션 개선

--- Programmers/lv3/이중우선순위큐/Solution.kt
+++ Programmers/lv3/이중우선순위큐/Solution.kt
@@ -1,85 +1,94 @@
-// [이중 우선순위 큐]
+// [이중우선순위큐]
 
-// 클래스
-// -- 멤버: 우선순위 큐 두개, 값 레코더 하나
-// -- maxPq, minPq, recorder
+// 값 입력
+// -- minPQ와 maxPQ에 동시 입력
 
-// 프로세스
-// -- 갑 입력/삭제 시 레코더 갱신
-// -- 삭제/읽기 과정에서 레코더에 0으로 기록된 값은 힙으로부터 미리 제거할 것 (cleanHeap)
+// 최대값 삭제
+// -- 최상위 값 조회 후 maxToDelete에 포함되는 값 연속 제거
+// -- maxPQ 값 삭제
+// -- minToDelete 값 추가
+
+// 최소값 삭제
+// -- 최상위 값 조회 후 minToDelete에 포함되는 값 연속 제거
+// -- minPQ 값 삭제
+// -- maxToDelete 값 추가
 
 import java.util.*
 
 class Solution {
-    private class Dpq {
-        private val minPq = PriorityQueue<Int>()
-        private val maxPq = PriorityQueue<Int>(compareBy { -it })
-        private val recorder = mutableMapOf<Int, Int>()
+    private class DPQ {
+        val maxPQ = PriorityQueue<Int>(compareBy { -it })
+        val minPQ = PriorityQueue<Int>()
 
-        private fun cleanHeap(pq: PriorityQueue<Int>) {
-            while (pq.isNotEmpty()) {
-                val top = pq.peek()
-                val count = recorder[top] ?: 0
-                if (count == 0) pq.poll()
-                else break
+        val maxToDelete = mutableMapOf<Int, Int>()
+        val minToDelete = mutableMapOf<Int, Int>()
+
+        private fun cleanHeap(pq: PriorityQueue<Int>, deleteMap: MutableMap<Int, Int>) {
+            while (maxPQ.isNotEmpty() && (deleteMap[pq.peek()] ?: 0) > 0) {
+                val removed = pq.poll()
+                deleteMap[removed] = deleteMap[removed]!! - 1
             }
         }
 
-        fun add(value: Int) {
-            minPq.add(value)
-            maxPq.add(value)
-            recorder.putIfAbsent(value, 0)
-            recorder[value] = recorder[value]!! + 1
+        fun add(n: Int) {
+            maxPQ.add(n)
+            minPQ.add(n)
         }
 
-        fun removeMin() {
-            cleanHeap(minPq)
-            if (minPq.isNotEmpty()) {
-                val value = minPq.poll()
-                recorder[value] = recorder[value]!! - 1
-            }
+        fun pollMax() {
+            if (maxPQ.isEmpty()) return
+
+            cleanHeap(maxPQ, maxToDelete)
+            val removed = maxPQ.poll()
+            minToDelete.putIfAbsent(removed, 0)
+            minToDelete[removed] = minToDelete[removed]!! + 1
         }
 
-        fun removeMax() {
-            cleanHeap(maxPq)
-            if (maxPq.isNotEmpty()) {
-                val value = maxPq.poll()
-                recorder[value] = recorder[value]!! - 1
-            }
+        fun pollMin() {
+            if (minPQ.isEmpty()) return
+
+            cleanHeap(minPQ, minToDelete)
+            val removed = minPQ.poll()
+            maxToDelete.putIfAbsent(removed, 0)
+            maxToDelete[removed] = maxToDelete[removed]!! + 1
         }
 
-        fun pollMin(): Int {
-            cleanHeap(minPq)
-            return if (minPq.isEmpty()) 0 else minPq.poll()
+        fun peekMax(): Int {
+            if (maxPQ.isEmpty()) return 0
+
+            cleanHeap(maxPQ, maxToDelete)
+            return maxPQ.peek()
         }
 
-        fun pollMax(): Int {
-            cleanHeap(maxPq)
-            return if (maxPq.isEmpty()) 0 else maxPq.poll()
+        fun peekMin(): Int {
+            if (minPQ.isEmpty()) return 0
+
+            cleanHeap(minPQ, minToDelete)
+            return minPQ.peek()
         }
     }
 
     fun solution(operations: Array<String>): IntArray {
-        val dpq = Dpq()
+        val dpq = DPQ()
         operations.forEach {
             val tokens = it.split(" ")
-            val command = tokens[0]
+            val cmd = tokens[0]
             val num = tokens[1].toInt()
 
-            when (command) {
+            when (cmd) {
                 "I" -> {
                     dpq.add(num)
                 }
 
-                "D" -> {
-                    if (num == 1) dpq.removeMax()
-                    else dpq.removeMin()
+                else -> {
+                    if (num == 1) dpq.pollMax()
+                    else dpq.pollMin()
                 }
             }
         }
 
-        val max = dpq.pollMax()
-        val min = dpq.pollMin()
+        val max = dpq.peekMax()
+        val min = dpq.peekMin()
         return intArrayOf(max, min)
     }
 }
