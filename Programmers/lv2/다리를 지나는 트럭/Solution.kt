2ae098ecb73b255b593aa885c9d00e76b40bfcc6
Jisu Kim <kjisubit@gmail.com>
2026-09-14 22:56:46 +0900

    refactor: 솔루션 개선

--- Programmers/lv2/다리를 지나는 트럭/Solution.kt
+++ Programmers/lv2/다리를 지나는 트럭/Solution.kt
@@ -1,40 +1,47 @@
-// FIFO 처리 -> 큐
-// 큐로 구현한 브릿지에 트럭 하나 올려놓고 시작
-// 시간이 지날 때마다 트럭 진입 가능 여부 체크
-// 트럭 인덱스가 끝짜락에 도달할 경우 종료
-// 브릿지에 남은 마지막 트럭 추출까지 걸린 시간 리턴
+// fifo -> 큐
+
+// bridge를 큐로 표현
+
+// truck_weights 순회
+// -- 최대 무게 넘기지 않는 선에서 FIFO 작업 수행
+// -- 현재 무게 실시간 갱신
 
 class Solution {
     fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
+        val bridge = ArrayDeque<Int>()
+        var truckIndex = 0
         var time = 0
         var bridgeWeight = 0
-        var truckIndex = 0
 
-        val bridgeQueue = ArrayDeque<Int>()
-        for (i in 0 until bridge_length) {
-            bridgeQueue.add(-1)
+        for (i in 1..bridge_length) {
+            bridge.addLast(-1)
         }
 
         while (truckIndex < truck_weights.size) {
             time++
 
-            val removed = bridgeQueue.removeFirst()
-            if (removed != -1) bridgeWeight -= truck_weights[removed]
+            val removedIndex = bridge.removeFirst()
+            if (removedIndex != -1) {
+                val removedTruckWeight = truck_weights[removedIndex]
+                bridgeWeight -= removedTruckWeight
+            }
 
             if (bridgeWeight + truck_weights[truckIndex] <= weight) {
-                bridgeQueue.addLast(truckIndex)
                 bridgeWeight += truck_weights[truckIndex]
+                bridge.addLast(truckIndex)
                 truckIndex++
             } else {
-                bridgeQueue.addLast(-1)
+                bridge.addLast(-1)
             }
         }
 
         while (bridgeWeight > 0) {
             time++
-
-            val removed = bridgeQueue.removeFirst()
-            if (removed != -1) bridgeWeight -= truck_weights[removed]
+            val removedIndex = bridge.removeFirst()
+            if (removedIndex != -1) {
+                val removedTruckWeight = truck_weights[removedIndex]
+                bridgeWeight -= removedTruckWeight
+            }
         }
 
         return time
