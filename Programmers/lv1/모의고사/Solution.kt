54bdbffe8f8fbece47090631953a965bad0f9554
Jisu Kim <kjisubit@gmail.com>
2026-09-19 21:18:16 +0900

    refactor: 솔루션 개선

--- Programmers/lv1/모의고사/Solution.kt
+++ Programmers/lv1/모의고사/Solution.kt
@@ -1,35 +1,36 @@
 // [모의고사]
 
-// 1. 수포자 순회
-// 2. 각 수포자 정답 카운트 후 어레이에 저장
+// 수포자 정답 패턴 별 answer 순회 후 정답 카운트 기록
 
 class Solution {
-    val patterns = arrayOf(
+    private val patterns = arrayOf(
         intArrayOf(1, 2, 3, 4, 5),
         intArrayOf(2, 1, 2, 3, 2, 4, 2, 5),
         intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
     )
 
-    private fun countAnswers(answers: IntArray, pattern: IntArray): Int {
-        var count = 0
-        answers.forEachIndexed { i, number ->
-            if (number == pattern[i % pattern.size]) count++
+    fun solution(answers: IntArray): IntArray {
+        var maxValues = Int.MIN_VALUE
+
+        var scoreArray = IntArray(3)
+        for (i in scoreArray.indices) {
+            scoreArray[i] = getScore(i, answers)
+            if (scoreArray[i] > maxValues) maxValues = scoreArray[i]
         }
-        return count
+
+        return scoreArray.indices
+            .filter { scoreArray[it] == maxValues }
+            .map { it + 1 }.toIntArray()
     }
 
-    fun solution(answers: IntArray): IntArray {
-        var maxValue = 0
-
-        val countArray = IntArray(3)
-        patterns.forEachIndexed { i, pattern ->
-            countArray[i] = countAnswers(answers, pattern)
-            if (countArray[i] > maxValue) maxValue = countArray[i]
+    private fun getScore(testerIndex: Int, answers: IntArray): Int {
+        var count = 0
+        val pattern = patterns[testerIndex]
+        for (i in answers.indices) {
+            val patternIndex = i % pattern.size
+            if (answers[i] == pattern[patternIndex]) count++
         }
 
-        return countArray.indices
-            .filter { countArray[it] == maxValue }
-            .map { it + 1 }
-            .sorted().toIntArray()
+        return count
     }
 }
